a7af6d24c9520e17e5ae45489f0d7f1ad9a77117
Apoorav.malik <apoorav.malikb49@gmail.com>
2023-05-05 14:49:37 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect, createContext } from "react";
+import axios from "axios";
+import "./App.css";
+import Coin from "./Coin";
+import ReactSwitch from "react-switch";
+import "./App.css";
+
+export const ThemeContext = createContext(null);
+
+function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  const [theme, setTheme] = useState("dark");
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={(theme, setTheme)}>
+      <div className="coin-app" id={theme}>
+        <div className="switch">
+          <ReactSwitch onChange={toggleTheme} checked = {theme === "dark"} />
+        </div>
+        <div className="coin-search">
+          <h1 className="coin-text">Search a currency</h1>
+          <form>
+            <input
+              className="coin-input"
+              type="text"
+              onChange={handleChange}
+              placeholder="Search"
+            />
+          </form>
+        </div>
+        {filteredCoins.map((coin) => {
+          return (
+            <Coin
+              key={coin.id}
+              name={coin.name}
+              price={coin.current_price}
+              symbol={coin.symbol}
+              marketcap={coin.total_volume}
+              volume={coin.market_cap}
+              image={coin.image}
+              priceChange={coin.price_change_percentage_24h}
+            />
+          );
+        })}
+      </div>
+    </ThemeContext.Provider>
+  );
+}
+
+export default App;
